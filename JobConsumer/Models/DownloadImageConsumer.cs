826a5df7b59a18bdccf7784de9c8f22bf4830a84
Sanzhar Yerzhanov <sanzar30062000@gmail.com>
2024-10-01 15:37:11 +0500

    adding serilog

--- JobConsumer/Models/DownloadImageConsumer.cs
+++ JobConsumer/Models/DownloadImageConsumer.cs
@@ -1,20 +1,19 @@
 using MassTransit;
+using Serilog;
+using Serilog.Core;
 
 namespace JobConsumer.Models;
 
 public class DownloadImageConsumer : IJobConsumer<DownloadImage>
 {
-    private readonly ILogger<DownloadImageConsumer> _logger;
     private readonly ImageStateDbContext _db;
-    public DownloadImageConsumer(ImageStateDbContext db, ILogger<DownloadImageConsumer> logger)
+    public DownloadImageConsumer(ImageStateDbContext db)
     {
         _db = db;
-        _logger = logger;
     }
     public async Task Run(JobContext<DownloadImage> context)
     {
-        await Task.Delay(TimeSpan.FromSeconds(6));
-        _logger.LogInformation("the document was loaded");
+        Log.Information("The document was loaded");
         var image = new Image()
         {
             ImageId = context.Job.ImageId,
@@ -22,5 +21,6 @@ public class DownloadImageConsumer : IJobConsumer<DownloadImage>
         };
         _db.Images.Add(image);
         await _db.SaveChangesAsync();
+        Log.Information("SAVED The document was saved to db");
     }
 }
--- JobConsumer/Program.cs
+++ JobConsumer/Program.cs
@@ -3,8 +3,21 @@ using MassTransit;
 using MassTransit.MessageData;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
 
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("MassTransit", LogEventLevel.Debug)
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.Hosting", LogEventLevel.Information)
+    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Command", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog();
 builder.Services.AddDbContext<ImageStateDbContext>(opts =>
 {
     opts.UseSqlServer(builder.Configuration.GetConnectionString("ImageService"));
@@ -17,19 +30,18 @@ builder.Services.AddMassTransit(x =>
         {
             e.ExistingDbContext<ImageStateDbContext>();
         });
+    x.AddDelayedMessageScheduler();
     x.UsingRabbitMq((context, cfg) =>
     {
         var directoryInfo = new DirectoryInfo(@"C:/Program Files/temp");
         cfg.UseMessageData(new FileSystemMessageDataRepository(directoryInfo));
+        cfg.UseDelayedMessageScheduler();
         cfg.Host("rabbitmq://localhost", h =>
         {
             h.Username(builder.Configuration["UserSettings:Username"]!);
             h.Password(builder.Configuration["UserSettings:Password"]!);
         });
-        cfg.ReceiveEndpoint("image-service", e =>
-        {
-            e.ConfigureConsumer<DownloadImageConsumer>(context);
-        });
+        
         cfg.ConfigureEndpoints(context);
     });
 });
